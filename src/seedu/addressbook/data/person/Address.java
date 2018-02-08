5a66b453686e996b9208603c2b49f77fdf934387
sarahgoh97 <sarahgohshining97@yahoo.com.sg>
2018-02-08 12:35:59 +0800

    Check Validity of Address Requires block number, street name, unit number and Postal Code

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -9,8 +9,9 @@ import seedu.addressbook.data.exception.IllegalValueException;
 public class Address {
 
     public static final String EXAMPLE = "123, some street";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
-    public static final String ADDRESS_VALIDATION_REGEX = ".+";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS =
+            "Person addresses require a block number, a street name, a unit number and a postal code";
+    public static final String ADDRESS_VALIDATION_REGEX = "(\\d{1,3}.)?.+\\s(\\d{6})$";
 
     public final String value;
     private boolean isPrivate;
