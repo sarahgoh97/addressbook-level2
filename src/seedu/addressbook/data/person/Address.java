9ded49170d225b9406f5a528daa3918025ed09db
sarahgoh97 <sarahgohshining97@yahoo.com.sg>
2018-02-09 15:33:00 +0800

    Changed Regex Expression for Address

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -10,8 +10,8 @@ public class Address {
 
     public static final String EXAMPLE = "123, some street";
     public static final String MESSAGE_ADDRESS_CONSTRAINTS =
-            "Person addresses require a block number, a street name, a unit number and a postal code";
-    public static final String ADDRESS_VALIDATION_REGEX = "(\\d{1,3}.)?.+\\s(\\d{6})$";
+            "Person addresses require a block number, a street name, a unit number and a 6-digit postal code number";
+    private static final String ADDRESS_VALIDATION_REGEX = "\\d+,.+,.+,.+(\\d{6}$)";
 
     public final String value;
     private boolean isPrivate;
