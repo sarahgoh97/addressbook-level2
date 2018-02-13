a0b446e10eabfd7dc970d303e7cd19af3d679782
sarahgoh97 <sarahgohshining97@yahoo.com.sg>
2018-02-13 18:35:00 +0800

    Added Contact superclass

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,55 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+/**
+ * Represents the contact information for a person
+ */
+
+public class Contact {
+    protected Person person;
+    private static String EXAMPLE;
+    private static String MESSAGE_CONSTRAINTS;
+    private static String VALIDATION_REGEX;
+
+    protected final String value;
+    private boolean isPrivate;
+
+    /**
+     * Validates given contact.
+     *
+     * @throws IllegalValueException if given contact is invalid.
+     */
+    public Contact(String example, String constraints, String regex, String received, boolean isPrivate) throws IllegalValueException {
+        EXAMPLE = example;
+        MESSAGE_CONSTRAINTS = constraints;
+        VALIDATION_REGEX = regex;
+        this.isPrivate = isPrivate;
+        String contact = received.trim();
+        if (!isValidContact(contact)) {
+            throw new IllegalValueException(constraints);
+        }
+        this.value = contact;
+    }
+
+    /**
+     * Returns true if the given string is a valid person email.
+     */
+    protected boolean isValidContact(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}
